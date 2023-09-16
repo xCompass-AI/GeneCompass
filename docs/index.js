e650809f041c1857bba19d24a7e45044c931a9ec
unknown <zlhu12@iflytek.com>
2023-09-16 19:20:42 +0800

    add readme and base intro

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-    document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+    document.getElementById('main-content').innerHTML = 'geneCompass'
 }
